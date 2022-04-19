9e16eb055ccd472f7472e8ff55c3756bb2bcfd52
johnnywwy <johnnywwy@gmail.com>
2022-04-19 18:24:57 +0800

    computed: ...mapState 用法

---
+++ src/components/Subtraction.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4>减法</h4>
+    <h4>当前count的值为：{{ count }}</h4>
+    <button>-1</button>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['count'])
+  }
+
+}
+</script>
